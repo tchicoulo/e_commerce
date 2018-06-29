b49ef5cc418a1533da9d9889462c911c3001a236
Guillaume <groult_guigui@hotmail.com>
2018-06-29 18:57:30 +0200

    adding link to home

--- views/navbar.php
+++ views/navbar.php
@@ -2,7 +2,7 @@
 
 
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="#"><img src="img/LOGO.jpg" width="30" height="30" class="d-inline-block align-top" alt="">Welcome To The Sheeks Store</a>
+			<a class="navbar-brand" href="/sheekstore/e_commerce/index.php/home"><img src="img/LOGO.jpg" width="30" height="30" class="d-inline-block align-top" alt="">Welcome To The Sheeks Store</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
