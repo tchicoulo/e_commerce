2b99d10dfbec3e92e54fa89b17888bfbea29aea5
Guillaume <groult_guigui@hotmail.com>
2018-06-30 02:59:31 +0200

    Adding model.php file for database connection

---
+++ models/model.php
@@ -0,0 +1,20 @@
+<?php
+class Model{
+	private $user = "root";
+  	private $password= "simplonco";
+	//private $password= "";
+  	private $dbname= "sheeks_ecommerce";
+	private $host= "localhost";
+	private $port= 8088;
+	public function connect(){
+		try{
+			      $db= new PDO ("mysql:host=".$this->host.";dbname=".$this->dbname,$this->user,$this->password);
+			      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			      return $db;
+		}
+		catch(PDOException $e) {
+			      echo "<span class=\"erreur\">Erreur de connexion à la base de données</span>";
+		}
+	}
+}
+?>
