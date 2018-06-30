b7c37669e8e3ceb97b66e245e24a3557442549ef
GuiGui <groult_guigui@hotmail.com>
2018-06-30 15:14:17 +0200

    Change

--- models/model.php
+++ models/model.php
@@ -1,9 +1,9 @@
 <?php
 class Model{
 	private $user = "root";
-  	private $password= "simplonco";
-	//private $password= "";
-  	private $dbname= "sheeks_ecommerce";
+  //private $password= "simplonco";
+	private $password= "";
+  private $dbname= "sheeks_ecommerce";
 	private $host= "localhost";
 	private $port= 8088;
 	public function connect(){
