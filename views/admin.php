484218945f9180e0d7ef8160bdb6f51b80210de1
Calixte <tchicoulo@hotmail.fr>
2018-07-02 17:04:16 +0200

    adding admin show clients

--- views/admin.php
+++ views/admin.php
@@ -47,7 +47,7 @@ else if($_SESSION['admin'] == 'yes'){ ?>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/sheekstore/e_commerce/index.php/admin/showusers">
+              <a class="nav-link" href="/sheekstore/e_commerce/index.php/admin/showclients">
                 <span data-feather="users"></span>
                 Utilisateurs
               </a>
@@ -105,7 +105,6 @@ else if($_SESSION['admin'] == 'yes'){ ?>
             </table>';
           }
           else if($action =='addproduct' || $action =='editproduct'){
-            global $categorie;
             echo '<form class="form-signin" action="/sheekstore/e_commerce/index.php/admin/showproducts" method="post">
             <h1 class="h3 mb-3 font-weight-normal">'.$verb.' un produit</h1>
             <input type="hidden" name="id" value="'.$id.'">
@@ -179,6 +178,45 @@ else if($_SESSION['admin'] == 'yes'){ ?>
               echo '<button class="btn btn-lg btn-primary btn-block" type="submit" name="update">Modifier</button>';
             }
             echo '</form>';
+          }
+
+          if($action == 'showclients' || $action == 'deleteclient'){
+            echo '<a href="/sheekstore/e_commerce/index.php/admin/addclient/"><button class="btn btn-lg btn-primary btn-block" name="addclient">Ajouter un client</button></a><br /><br />';
+
+            if(isset($result)){ echo $result; } // Affichage du message de confirmation / erreur
+
+            echo '<table class="table table-striped table-sm">
+            <thead>
+            <tr>
+            <th>Id</th>
+            <th>Nom client</th>
+            <th>Prénom</th>
+            <th>Nom</th>
+            <th>Email</th>
+            <th>Admin</th>
+            <th>Editer</th>
+            <th>Supprimer</th>
+            </tr>
+            </thead>
+            <tbody>
+            ';
+
+            foreach($ClientsListView as $client){
+              echo '<tr>
+              <td>'.$client['id'].'</td>
+              <td><a href="/sheekstore/e_commerce/index.php/admin/showclients/'.$client['id'].'">'.$client['nom_client'].'</a></td>
+              <td>'.$client['prenom'].'</td>
+              <td>'.$client['nom'].'</td>
+              <td>'.$client['email'].'</td>
+              <td>'.$client['admin'].'</td>
+              <form method="post">
+              <td><input type="image" formaction="/sheekstore/e_commerce/index.php/admin/editclient/'.$client['id'].'" src="/sheekstore/e_commerce/img/edit.png" alt="Icone d\'édition" class="icon" /></td>
+              <td><input type="image" formaction="/sheekstore/e_commerce/index.php/admin/deleteclient/'.$client['id'].'" src="/sheekstore/e_commerce/img/delete.png" alt="Icone de suppression" class="icon" /></td>
+              </form>';
+            }
+
+            echo '</tbody>
+            </table>';
           }
         }
         else{
