8c61f1ecc3200509ee80f15c7558872255b47f9b
Steven <tchicoulo@hotmail.fr>
2018-07-03 16:06:55 +0200

    Panel admin/CRUD orders

--- controllers/admin.php
+++ controllers/admin.php
@@ -115,6 +115,65 @@ else if(isset($action) && $action == 'deletecategory'){
 
   $adminList= $category->getAll();
 }
+
+// ORDERS
+if(isset($action) && $action == 'showorders'){
+  // Condition ternaire pour affecter les valeur envoyer via la methode POST, on leur affecte une valeur par défaut le cas échéant
+  $id = (isset($_POST['id'])) ?  $_POST['id'] : 0;
+  $date_commande = (isset($_POST['date_commande'])) ?  $_POST['date_commande'] : 'default';
+  $id_client = (isset($_POST['id_client'])) ?  $_POST['id_client'] : 'default';
+
+  // Création d'un objet ProductsModel
+  $product =new OrdersModel(['id' => $id ,'date_commande' => $date_commande, 'id_client' => $id_client]);
+
+  if(isset($_POST['add'])){	 // Ajout d'un produit
+    $result = $order->create($order);
+  }
+
+  else if(isset($_POST['update'])){ // Mise à jour d'un produit
+    $result = $order->update($order);
+  }
+
+  $adminList= $order->getAll();
+}
+else if(isset($action) && $action == 'addorder'){
+  $category = new CategoriesModel(['id' => 0 ,'nom_categorie' => '']);
+  $categories = $category->getAll();
+
+  // setting defaults values
+  $verb = 'Ajouter';
+  $id = 0;
+  $date_commande = '';
+  $id_client = '';
+}
+
+else if(isset($action) && $action == 'editorder'){
+  $category = new CategoriesModel(['id' => 0 ,'nom_categorie' => '']);
+  $order =new OrdersModel(['id' => 0 ,'date_commande' => '', 'id_client' => '']);
+
+  $categories = $category->getAll();
+
+  if($method == 'POST' && isset($args[0])){
+    $product = $product->get((int)$args[0]);
+
+    // setting order values
+    $verb = 'Modifier';
+    $id = $order->id();
+    $date_commande = $order->date_commande();
+    $id_client = $order->id_client();
+  }
+}
+
+else if(isset($action) && $action == 'deleteproduct'){
+  $order =new OrdersModel(['id' => 0 ,'date_commande' => '', 'id_client' => '']);
+
+  if($method == 'POST' && isset($args[0])){
+    $result = $order->delete((int)$args[0]); // Suppression d'une commande
+  }
+
+  $adminList= $order->getAll();
+}
+
 // CLIENTS
 
 if (isset($action) && $action == "showclients") {
--- views/admin.php
+++ views/admin.php
@@ -133,6 +133,68 @@ else if($_SESSION['admin'] == 'yes'){ ?>
             echo '</form>';
           }
 
+          // ORDERS
+          if($action == 'showorders' || $action == 'deleteorder'){
+            echo '<a href="/sheekstore/e_commerce/index.php/admin/addorder/"><button class="btn btn-lg btn-primary btn-block" name="addorder">Ajouter une commande</button></a><br /><br />';
+
+            if(isset($result)){ echo $result; } // Affichage du message de confirmation / erreur
+
+            echo '<table class="table table-striped table-sm">
+            <thead>
+            <tr>
+            <th>Id</th>
+            <th>Date commande</th>
+            <th>Id client</th>
+            <th>Editer</th>
+            <th>Supprimer</th>
+            </tr>
+            </thead>
+            <tbody>
+            ';
+
+            foreach($adminList as $order){
+              echo '<tr>
+              <td>'.$order['id'].'</td>
+              <td><a href="/sheekstore/e_commerce/index.php/admin/showorders/'.$order['id'].'">'.$order['date_commande'].'</a></td>
+              <td>'.$order['id_client'].'</td>
+              <form method="post">
+              <td><input type="image" formaction="/sheekstore/e_commerce/index.php/admin/editorder/'.$order['id'].'" src="/sheekstore/e_commerce/img/edit.png" alt="Icone d\'édition" class="icon" /></td>
+              <td><input type="image" formaction="/sheekstore/e_commerce/index.php/admin/deleteorder/'.$order['id'].'" src="/sheekstore/e_commerce/img/delete.png" alt="Icone de suppression" class="icon" /></td>
+              </form>';
+            }
+
+            echo '</tbody>
+            </table>';
+          }
+          else if($action =='addorder' || $action =='editorder'){
+            echo '<form class="form-signin" action="/sheekstore/e_commerce/index.php/admin/showorders" method="post">
+            <h1 class="h3 mb-3 font-weight-normal">'.$verb.' une commande</h1>
+            <input type="hidden" name="id" value="'.$id.'">
+            <label for="inputName" class="sr-only">Date de la commande</label>
+            <input type="text" class="form-control" placeholder="Date de la commande" name="date_commande" value="'.$date_commande.'" required autofocus><br/>
+            <label for="inputBrand" class="sr-only">Id client</label>
+            <input type="text" class="form-control" placeholder="Id client" name="id_client" value="'.$id_client.'" required><br/>
+            <label for="inputCategory" class="sr-only">Catégorie</label>
+            <h5>Categorie:</h5>
+            <select class="form-control" name="id_Categorie">';
+            foreach($categories as $category){
+              if($id_categorie == $category['id']){
+                echo '<option value="'.$category['id'].'" selected>'.$category['nom_categorie'].'</option>';
+              }
+              else{
+                echo '<option value="'.$category['id'].'">'.$category['nom_categorie'].'</option>';
+              }
+            }
+            echo '</select><br /><br />';
+            if($action == 'addorder'){
+              echo '<button class="btn btn-lg btn-primary btn-block" type="submit" name="add">Ajouter</button>';
+            }
+            else if($action == 'editorder'){
+              echo '<button class="btn btn-lg btn-primary btn-block" type="submit" name="update">Modifier</button>';
+            }
+            echo '</form>';
+          }
+
           // CATEGORIES
           if($action == 'showcategories' || $action == 'deletecategory'){
             echo '<a href="/sheekstore/e_commerce/index.php/admin/addcategory/"><button class="btn btn-lg btn-primary btn-block" name="addcategory">Ajouter une Catégorie</button></a><br /><br />';
