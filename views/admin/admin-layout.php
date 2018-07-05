5da05ad0dd4ee7ec2a5d44b95318bdca84c028a6
Guillaume <groult_guigui@hotmail.com>
2018-07-05 18:12:43 +0200

    resolving javascript link issue

--- views/admin/admin-layout.php
+++ views/admin/admin-layout.php
@@ -28,12 +28,6 @@
 	require_once $content;
 
 	?>
-
-
-
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
   
   <!-- Icons -->
   <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
@@ -79,6 +73,7 @@
     <script src="/sheekstore/e_commerce/js/classy-nav.min.js"></script>
     <script src="/sheekstore/e_commerce/js/map-active.js"></script>
     <script src="/sheekstore/e_commerce/js/plugins.js"></script>
+    <script src="/sheekstore/e_commerce/js/main.js"></script>
     <script src="/sheekstore/e_commerce/js/main.js"></script>
 </body>
 </html>
