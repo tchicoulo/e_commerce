f2984632b6fdf66cfaedfccd6d7c2a47a808ef40
BoRNToC0De <borntocode.simplon@gmail.com>
2018-07-06 20:06:28 +0200

    imageresizing

--- views/home.php
+++ views/home.php
@@ -170,13 +170,13 @@
                                         <div class="hover-content">
                                             <!-- Add to Cart -->
                                             <div class="add-to-cart-btn">
-																								<input name="id_product" type="hidden" value="'.$product['id'].'">
+												<input name="id_product" type="hidden" value="'.$product['id'].'">
                                                 <button type="submit" class="btn essence-btn">Ajouter au panier</button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-																</form>
+									</form>
                             </div>';
                            }
                         ?>
