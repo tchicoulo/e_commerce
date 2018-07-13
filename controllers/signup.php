8099ce984804fc50f7146a66c66c1c366b09842f
Guillaume <groult_guigui@hotmail.com>
2018-07-13 15:16:00 +0200

    resolving issue with password confirm

--- controllers/signup.php
+++ controllers/signup.php
@@ -6,19 +6,24 @@ if(isset($_POST['signup'])){
 	$login    = htmlspecialchars($_POST['login']);
 	$password = htmlspecialchars(password_hash($_POST['password'], PASSWORD_DEFAULT));
 	$email    = htmlspecialchars($_POST['email']);
+	$passconfirm = htmlspecialchars(password_hash($_POST['confirm'], PASSWORD_DEFAULT));
 
 
+		if(htmlspecialchars($_POST['password']) == htmlspecialchars($_POST['confirm'])){
+			$client =new ClientsModel(['id' => 0 ,'nom_client' => $login, 'mot_de_passe' => $password, 'civilite' => '', 'prenom' => '', 'nom' => '', 'adresse' => '', 'telephone' => '', 'email' => $email ]);
 
-	$client =new ClientsModel(['id' => 0 ,'nom_client' => $login, 'mot_de_passe' => $password, 'civilite' => '', 'prenom' => '', 'nom' => '', 'adresse' => '', 'telephone' => '', 'email' => $email ]);
+			$result = $client->create($client);
 
-	$result = $client->create($client);
-
-	if($result == '<p class="green">L\'utilisateur '.$client->nom_client().' à bien été ajouté.</p>'){
-		$_SESSION['login'] = $client->nom_client();
-		$_SESSION['email'] = $client->email();
-		$_SESSION['admin'] = $client->admin();
+			if($result == '<p class="green">L\'utilisateur '.$client->nom_client().' à bien été ajouté.</p>'){
+				$_SESSION['login'] = $client->nom_client();
+				$_SESSION['email'] = $client->email();
+				$_SESSION['admin'] = $client->admin();
+			}
+		}
+		else{
+			$result = '<p class="red">Les mots de passe de correspondent pas</p>';
+		}
 	}
-}
 
-header("Location: ./home");
-?>
+	header("Location: ./home");
+	?>
